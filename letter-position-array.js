4715760e1cfc1aee78e4a7c430539180847428d5
Zushisan <74838@supinfo.com>
2018-01-17 01:26:50 +0000

    Trying to fix bug and adding a version with the object keys refering to arrays with the different position of the letter

---
+++ letter-position-array.js
@@ -0,0 +1,49 @@
+
+function positionLetters(string){
+
+  var letters = string.split(" ").join(" ").toLowerCase();
+
+  console.log(letters);
+  var output = {};
+
+
+  // for ( var letter of letters){
+
+  //   if (letter.match(/[a-z]/i)){
+
+  //     if (output[letter]){
+
+  //       output[letter] += output[letter] + ", ";
+
+  //     }
+
+  //     else {
+  //       output[letter] = ;
+  //     }
+  //   }
+  // }
+
+  for (var i = 0; i < letters.length; i++){
+
+
+
+    if (letters[i].match(/[a-z]/i)){
+
+      if (output[letters[i]]){
+
+        output[letters[i]].push(i);
+
+      }
+      else {
+
+        output[letters[i]] = [i];
+
+      }
+    }
+  }
+
+return output;
+
+}
+
+console.log(positionLetters("My name is Romain."));
